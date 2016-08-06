15bf276f4d9fab3a27027638b654c77a8e72ce48
xgerrmann <xander@gerrmann.nl>
2016-08-06 09:58:58 +0200

    Remove upsampling from detector. Finish implementing downsampling and of detection frame and scaling the results.

--- main.py
+++ main.py
@@ -4,28 +4,56 @@ import cv2
 import dlib
 import numpy as np
 from paths import trained_model
+from datetime import datetime
+from time import time
 
-def show_landmarks(win,predictor,detector,frame):
+## Sources
+# Example:		http://dlib.net/face_landmark_detection.py.html
+# Speeding up:	http://www.learnopencv.com/speeding-up-dlib-facial-landmark-detector/
+
+
+def detect(win,predictor,detector,frame):
 	# Ask the detector to find the bounding boxes of each face. The 1 in the
 	# second argument indicates that we should upsample the image 1 time. This
 	# will make everything bigger and allow us to detect more faces.
-	dets = detector(frame, 1)
+	# Draw new frame
+	temp = time()
+	win.set_image(frame)
+	print 'Plot frame: ',time() - temp, 's'
+	# Detect face
+	temp = time()
+	# subsampling for faster detection
+	subsample = 2.0
+	frame_subsample = cv2.resize(frame, (0,0), fx=1/subsample, fy=1/subsample)
+	dets = detector(frame_subsample, 0)
+	print 'Detect: ',time() - temp, 's'
+	if len(dets) == 0: # do nothing if no face has been detected
+		return 0
 	print("Number of faces detected: {}".format(len(dets)))
 	# TODO: use only face with highest detection strength: other faces should be ignored
-	print dets
+	# TODO: make a model of the location of the face for faster detection
+	# Rescale detected recangle in subsampled image
+	left	= int(dets[0].left()*subsample)
+	top		= int(dets[0].top()*subsample)
+	right	= int(dets[0].right()*subsample)
+	bottom	= int(dets[0].bottom()*subsample)
+	d		= dlib.rectangle(left,top,right,bottom)
+	print("Left: {} Top: {} Right: {} Bottom: {}".format(
+		d.left(), d.top(), d.right(), d.bottom()))
+	# Get the landmarks/parts for the face in box d.
+	temp = time()
+	shape = predictor(frame, d)
+	print 'Predict: ',time() - temp, 's'
+	# Clear overlay
+	temp = time()
 	win.clear_overlay()
-	if len(dets)>0:
-		d	= dets[0]
-		k	= 0
-		#print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
-		#	k, d.left(), d.top(), d.right(), d.bottom()))
-		# Get the landmarks/parts for the face in box d.
-		shape = predictor(frame, d)
-		#print("Part 0: {}, Part 1: {} ...".format(shape.part(0),shape.part(1)))
-		# Draw the face landmarks on the screen.
-		win.add_overlay(shape)
-	win.set_image(frame)
-	#print np.array(dets)
+	print 'Clear overlay: ',time() - temp, 's'
+	# Draw the face landmarks on the screen.
+	temp = time()
+	win.add_overlay(shape)
+	print 'Add overlay: ',time() - temp, 's'
+
+	return 0
 
 def main():
 	detector	= dlib.get_frontal_face_detector()
@@ -45,8 +73,7 @@ def main():
 		key = cv2.waitKey(1)
 		if key == 27: # exit on ESC
 			break
-		show_landmarks(win,predictor,detector,frame)
-		#dlib.hit_enter_to_continue()
+		detect(win,predictor,detector,frame)
 	vc.release()
 	#cv2.destroyWindow("preview")
 
