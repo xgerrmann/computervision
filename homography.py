502c087338f9982c4c9d68eb54d34c75acd3d3f2
xgerrmann <xander@gerrmann.nl>
2016-08-08 11:05:48 +0200

    Add larger example image. Also make seperate script for developping homography code.

--- homography.py
+++ homography.py
@@ -1,98 +1,17 @@
 #!/usr/bin/python
+# homography.py
+# script to test and perform homographies on an example image
 
 import cv2
-import dlib
 import numpy as np
-from paths import trained_model, default_image
-from datetime import datetime
-from time import time
+from paths import default_image
 import math
-from utils import pose
 import sys
 import copy
 
 ## Sources
 # Example:		http://dlib.net/face_landmark_detection.py.html
 # Speeding up:	http://www.learnopencv.com/speeding-up-dlib-facial-landmark-detector/
-
-def detect(win,predictor,detector,frame):
-	# Ask the detector to find the bounding boxes of each face. The 1 in the
-	# second argument indicates that we should upsample the image 1 time. This
-	# will make everything bigger and allow us to detect more faces.
-	# Draw new frame
-	temp = time()
-	win.set_image(frame)
-	print 'Plot frame: ',time() - temp, 's'
-	# Detect face
-	temp = time()
-	# subsampling for faster detection
-	subsample = 2.0
-	frame_subsample = cv2.resize(frame, (0,0), fx=1/subsample, fy=1/subsample)
-	dets = detector(frame_subsample, 0)
-	print 'Detect: ',time() - temp, 's'
-	if len(dets) == 0: # do nothing if no face has been detected
-		return 0
-	print("Number of faces detected: {}".format(len(dets)))
-	# TODO: use only face with highest detection strength: other faces should be ignored
-	# TODO: make a model of the location of the face for faster detection
-	# Rescale detected recangle in subsampled image
-	left	= int(dets[0].left()*subsample)
-	top		= int(dets[0].top()*subsample)
-	right	= int(dets[0].right()*subsample)
-	bottom	= int(dets[0].bottom()*subsample)
-	d		= dlib.rectangle(left,top,right,bottom)
-	print("Left: {} Top: {} Right: {} Bottom: {}".format(
-		d.left(), d.top(), d.right(), d.bottom()))
-	# Get the landmarks/parts for the face in box d.
-	temp = time()
-	shape = predictor(frame, d)
-	print 'Predict: ',time() - temp, 's'
-	# Clear overlay
-	temp = time()
-	win.clear_overlay()
-	print 'Clear overlay: ',time() - temp, 's'
-	# Draw the face landmarks on the screen.
-	temp = time()
-	win.add_overlay(shape)
-	print 'Add overlay: ',time() - temp, 's'
-
-	return shape
-
-# TODO: draw in openCV and not wit dlib (shape.parts())
-def shape2pose(shape_calibrated, shape_current):
-# This function determines the pose of the head by using the foudn markers on the face.
-# The current markers are compared to the calibrated markers and from this the head pose is determined.
-# Currently only the nos bridge length is used, which is only to allow for a proof of concept.
-# Definition of head pose: x, y, z, rx (pitch), ry (yaw), rz (roll) -> http://gi4e.unavarra.es/databases/hpdb/
-	# Initialization of head pose
-	headpose = pose()
-
-	pts		= shape_current.parts()
-	#for pt in pts:
-	#	print pt
-	nose_bridge	= pts[27:31]
-	jaw_line	= pts[0:16]
-	
-	# TODO: no rotation is assumed
-	ymin = np.inf
-	ymax = -1
-	for pt in nose_bridge:
-		x = pt.x
-		y = pt.y
-		if y<ymin:
-			ymin = y
-		if y>ymax:
-			ymax = y
-	# TODO: use calibrated shape do determine normal nose bridge length
-	nb_length_cal = 45
-	nb_length_cur = ymax - ymin
-
-	headpose.rx = math.cos(float(nb_length_cur)/float(nb_length_cal))
-	print nb_length_cur
-	print nb_length_cal
-	print	math.acos(float(nb_length_cur)/float(nb_length_cal))
-
-	return headpose
 
 def calcrotationmatrix(rx, ry, rz):
 	# source: http://nghiaho.com/?page_id=846
@@ -165,7 +84,7 @@ def getoutputimagesize(Tpers,homography,height,width):
 	print 'Size image_out: ',height_out,', ',width_out
 	return (height_out, width_out)
 
-def adjustwindow(windowname,image,headpose):
+def performhomography(windowname,image,headpose):
 	rx	= 0.0*math.pi
 	ry	= 0.0*math.pi
 	rz	= 0.0*math.pi
@@ -286,42 +205,15 @@ def adjustwindow(windowname,image,headpose):
 	return 0
 
 def main():
-	detector	= dlib.get_frontal_face_detector()
-	print trained_model
-	predictor	= dlib.shape_predictor(trained_model)
 	image		= cv2.imread(default_image)
-	windowname = "image"
+	windowname	= "image"
 	cv2.namedWindow(windowname)
 	cv2.imshow(windowname,image)
 	cv2.waitKey(1)
 	windowname = 'test'
 	cv2.namedWindow(windowname)
-	vc = cv2.VideoCapture(0)
-	win_dlib = dlib.image_window()
-	if vc.isOpened(): # try to get the first frame
-		rval, frame = vc.read()
-	else:
-		rval = False
-
-	while rval:
-		#cv2.imshow("preview", frame)
-		rval, frame = vc.read()
-		key = cv2.waitKey(1)
-		if key == 27: # exit on ESC
-			break
-		shape = detect(win_dlib,predictor,detector,frame)
-		if shape == 0:
-			continue
-		else:
-			# TODO: calibrated shape is now same as current, calibrated shape must be indicated by used.
-			shape_calibrated= shape
-			shape_current	= shape
-			headpose		= shape2pose(shape_calibrated, shape_current)
-			print headpose
-			adjustwindow(windowname, image, headpose)
-
-	vc.release()
-	#cv2.destroyWindow("preview")
+	
+	performhomography(windowname, image)
 
 	return 0
 
--- paths.py
+++ paths.py
@@ -4,6 +4,8 @@ trained_model = 'shape_predictor_68_face_landmarks.dat'
 #default_image = 'evening_romantic.jpg'
 # source: http://a1.mzstatic.com/us/r30/Purple5/v4/5a/2e/e9/5a2ee9b3-8f0e-4f8b-4043-dd3e3ea29766/icon256.png
 #default_image = 'icon256.png'
+# source: https://www.google.nl/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiMs-aspK_OAhXCthoKHYiqAh4QjRwIBw&url=http%3A%2F%2Fmafiawars.wikia.com%2Fwiki%2FUser_blog%3AToenailsin%2FRandom_Snaps&bvm=bv.129391328,d.d2s&psig=AFQjCNH0H2XuUSLG_WqjLTkYrv6-tSzBIw&ust=1470658566944464
+default_image = '50x50.png'
 # source: http://www.ramclutches.com/social_media.html
-default_image = 'icon_small.png'
+#default_image = 'icon_small.png'
 
