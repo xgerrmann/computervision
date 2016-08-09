4d2b6621e086e4a5ebef7ffa24b7b5306ab739f7
xgerrmann <xander@gerrmann.nl>
2016-08-09 18:34:47 +0200

    Placeeach homography method in a separate function.

--- homography.py
+++ homography.py
@@ -1,230 +1,270 @@
 #!/usr/bin/python
 # homography.py
 # script to test and perform homographies on an example image
-
+# script based on: http://math.stackexchange.com/questions/494238/how-to-compute-homography-matrix-h-from-corresponding-points-2d-2d-planar-homog/1886060#1886060
 import cv2
 import numpy as np
 from paths import default_image
 import math
 import sys
 import copy
-
-## Sources
-# Example:		http://dlib.net/face_landmark_detection.py.html
-# Speeding up:	http://www.learnopencv.com/speeding-up-dlib-facial-landmark-detector/
+import time
 
 def calcrotationmatrix(rx, ry, rz):
 	# source: http://nghiaho.com/?page_id=846
 	# source: https://en.wikipedia.org/wiki/3D_projection (uses negative angles?)
-	rx = -rx
-	ry = -ry
-	rz = -rz
-	X	= np.matrix([[1, 0, 0],
+#	rx = -rx
+#	ry = -ry
+#	rz = -rz
+	Rx	= np.matrix([[1, 0, 0],
 					[0,	math.cos(rx), -math.sin(rx)],
 					[0, math.sin(rx), math.cos(rx)]])
-	Y	= np.matrix([[math.cos(ry), 0, math.sin(ry)],
+	Ry= np.matrix([[math.cos(ry), 0, math.sin(ry)],
 					[0, 1, 0],
 					[-math.sin(ry), 0, math.cos(ry)]])
-	Z	= np.matrix([[math.cos(rz), -math.sin(rz), 0],
+	Rz	= np.matrix([[math.cos(rz), -math.sin(rz), 0],
 					[math.sin(rz), math.cos(rz), 0],
 					[0, 0, 1]])
-	#R	= Z*Y*X
-	R	= X*Y*Z
-	return R
+	return (Rx,Ry,Rz)
 
-def getoutputimagesize(Tpers,homography,height,width):
-	# Corner points : [x,y,w]
-#	p0 = np.matrix([[0],[0],[1]])
-#	p1 = np.matrix([[width],[0],[1]])
-#	p2 = np.matrix([[0],[height],[1]])
-#	p3 = np.matrix([[width],[height],[1]])
-	# with z coordinate = 0
-	# w = 1
-	p0 = np.matrix([[0],[0],[0],[1]])
-	p1 = np.matrix([[width],[0],[0],[1]])
-	p2 = np.matrix([[0],[height],[0],[1]])
-	p3 = np.matrix([[width],[height],[0],[1]])
-	cornerpoints = [p0,p1,p2,p3]
-	xmin = np.inf
-	xmax = -np.inf
-	ymin = np.inf
-	ymax = -np.inf
-	for p in cornerpoints:
-		res = Tpers*homography*p
-		print p
-		print res
-		x_tmp = res[0]
-		y_tmp = res[1]
-		if x_tmp<xmin:
-			xmin=x_tmp
-		if y_tmp<ymin:
-			ymin=y_tmp
-		if x_tmp>xmax:
-			xmax=x_tmp
-		if y_tmp>ymax:
-			ymax=y_tmp
-	height_out	= int(np.ceil(ymax-ymin))
-	width_out	= int(np.ceil(xmax-xmin))
-	print 'Size image_out: ',height_out,', ',width_out
-	return (height_out, width_out)
-
-def calctransmatrix(e):
-	ex = e[0]
-	ey = e[1]
-	ez = e[2]
-
-	# T: transformation matrix for projection of 3D -> 2D
-	T = np.zeros((4,4))
-	T[0,0] = 1
-	T[1,1] = 1
-	T[2,2] = 1
-	T[0,2] = -ex/ez
-	T[1,2] = -ey/ez
-	T[3,2] = 1/ez
-	for i in range(4):
-		for j in range(4):
-			print '%6.6f'%float(T[i,j])
-	return T
-
-def performhomography(windowname,image):
-	rx	= 0.0*math.pi
-	ry	= 0.0*math.pi
-	rz	= 0.0*math.pi
-	R	= calcrotationmatrix(rx, ry, rz)
-	
+def calchomography(image):
 	pitch = 0.2625*(10**-3) # [m] pixel pitch (pixel size) assume square pixels, which is generally true
-	print 'Pitch: %.6f'%pitch
+	#print 'Pitch: %.6f'%pitch
 	
-#	tx	= 0 # translation x
-#	ty	= 0 # translation y
-#	tz	= 0 # translation z
+	rx = 0.2*math.pi
+	ry = 0.0*math.pi
+	rz = 0.*math.pi
 
-	f = -1 # TODO: must be detected accurately for correct results
-	
-	# e: viewers position relative to the camera
-	ex = 0
-	ey = 0
-	ez = -f
-	e = np.matrix([[ex],[ey],[ez]]) # viewer is direcly behind projection plane
+	(height,width,channels)	= image.shape
+	(Rx,Ry,Rz)		= calcrotationmatrix(rx,ry,rz)
+	Rt	= Rz*Ry*Rx
+	Rti	= np.linalg.inv(Rt)
+	# define 3 points on the virtual image plane
+	p0 = np.matrix([[0],[0],[0]])
+	p1 = np.matrix([[1],[0],[0]])
+	p2 = np.matrix([[0],[1],[0]])
+	# preform rotation of points
+	pr0 = Rt*p0
+	pr1 = Rt*p1
+	pr2 = Rt*p2
+	# find 2 vectors that span the plane:
+	# pr0 is always <0,0,0>, so the vectors pr1 and pr2 define the plane
 
-	T = calctransmatrix(e)
-
-	print 'Rotation matrix: \n',R
-	print 'Transformation matrix: \n',T
-	
-#	hom_inv		= np.linalg.inv(homography)
-#	print 'Inverse homography:\n',hom_inv
-#	print image.shape
-	(height, width, channels) = image.shape
-
-	# determine size of resulting image:
-	#(height_out, width_out) = getoutputimagesize(T,R,height,width)
-	height_out	= height
-	width_out	= width
-	w = 1
-	X = np.zeros((height_out,width_out))
-	Y = np.zeros((height_out,width_out))
-	Y = np.zeros((height_out,width_out))
-	W = np.zeros((height_out,width_out))
-	for xp in range(width_out):
+	# construct the vectors for the view-lines from the optical center to the corners of the virtual image:
+	# Corner numbering:
+	# 0-----1
+	# |		|
+	# 3-----2
+	# pixels
+	# corner [x,y]
+	cp0	= np.matrix([[0],[0]])
+	cp1	= np.matrix([[width],[0]])
+	cp2	= np.matrix([[width],[height]])
+	cp3	= np.matrix([[0],[height]])
+	corners_p = [cp0, cp1,cp2,cp3]
+	#print corners_p
+	# meters
+	f	= 0.3 # [m]
+	e	= np.matrix([[0],[0],[f]]) # position of eye
+	#print e
+	wx	= width*pitch	# [m]
+	hy	= height*pitch	# [m]
+	c0	= np.matrix([[-wx/2],[+hy/2],[0]])-e # vector from eye to corner 0
+	c1	= np.matrix([[+wx/2],[+hy/2],[0]])-e # vector from eye to corner 1
+	c2	= np.matrix([[+wx/2],[-hy/2],[0]])-e # vector from eye to corner 2
+	c3	= np.matrix([[-wx/2],[-hy/2],[0]])-e # vector from eye to corner 3
+	cornerlines = [c0,c1,c2,c3]
+	#print 'Lines:\n',cornerlines
+	# For each intersection a linear combination of the vectors spanning the plane exists
+	# when this combination is found, the exact location of the intersection is known
+	corners_proj = []
+	for ic, c in enumerate(cornerlines):
+		# find the projection of each corner point on the plane
+		# note: origin is still center of the plane
+		A	= np.hstack((c,-pr1,-pr2))
+		Ai	= np.linalg.inv(A)
+		#print A
+		#print Ai
+		#print 'Corner: \n',c
+		comb = Ai*(-e)
+		#print comb
+		intersection = np.hstack((pr1,pr2))*comb[1:]
+		#print intersection
+		#print 'Intersection:\n', intersection
+		# compute x,y coordinates in plane by performing the inverse plane rotation on the point of intersection
+		coords = Rti*intersection
+		#print 'Coordinates:\n',coords
+		x = (coords[0]+wx/2)/pitch
+		y = -(coords[1]-hy/2)/pitch # change y direction
 		#print x
-		for yp in range(height_out):
-			# a is the position of the point to be projected
-			# x and y depend on pixel size (screen dependent)
-			# also set origin to center of image
-			x = (xp-width_out/2)*pitch
-			y = (yp-height_out/2)*pitch
-			#a = np.matrix([[x],[y],[0]]) # virtual image is always at z=0
-			a = np.matrix([[x],[y],[0.5]]) # virtual image is always at z=0
-			
-			print 'a: ',a
-			# c is the 3D position of the camera
-			c = np.matrix([[0],[0],[1]])
-			print 'c: ',c
-			# d is the position of point A with respect to a coordinate system
-			dtmp = R*(a-c)
-			# convert d to homogeneous coordinates
-			print dtmp
-			d = np.matrix(np.zeros((4,1)))
-			d[0:3,0]= dtmp[0:3,0]
-			d[3,0]	= 1
-			print d
-			# 3D -> 2D
-			f = T*d
-			#print 'f: ',f
-			fx = f[0]
-			fy = f[1]
-			fz = f[2]
-			fw = f[3]
-			print 'fx: ',float(fx)
-			print 'fy: ',float(fy)
-			print 'fw: ',float(fw)
-			if fw<>0.0: # TODO: why is fw always zeros? only in equal plane scenario?
-			#	print 'True'
-				sys.exit('Weird')
-				bx = fx/fw
-				by = fy/fw
+		#print y
+		corners_proj.append([float(x),float(y)])
+		#corners_proj[0,ic]=x
+		#corners_proj[1,ic]=y
+
+	# projected corners is in pixels
+	#print 'Projected corners:\n',corners_proj
+	
+	xmin_out	= np.inf
+	ymin_out	= np.inf
+	xmax_out	= -np.inf
+	ymax_out	= -np.inf
+	for corner_proj in corners_proj:
+		x = corner_proj[0]
+		y = corner_proj[1]
+		if x < xmin_out:
+			xmin_out = x
+		if x > xmax_out:
+			xmax_out = x
+		if y < ymin_out:
+			ymin_out = y
+		if y > ymax_out:
+			ymax_out = y
+	xmin_out = int(np.ceil(xmin_out))
+	ymin_out = int(np.ceil(ymin_out))
+	xmax_out = int(np.ceil(xmax_out))
+	ymax_out = int(np.ceil(ymax_out))
+	height_out	= int(np.ceil(ymax_out - ymin_out))
+	width_out	= int(np.ceil(xmax_out - xmin_out))
+
+	# corners in the projection are now known.
+	# calculate the homography
+	# source: http://math.stackexchange.com/questions/494238/how-to-compute-homography-matrix-h-from-corresponding-points-2d-2d-planar-homog
+	M1 = np.zeros((0,8))
+	M2 = np.zeros((8,1))
+	for i in range(4):
+		xA	= float(corners_p[i][0])
+		yA	= float(corners_p[i][1])
+		xB	= float(corners_proj[i][0])
+		yB	= float(corners_proj[i][1])
+		#print 'xA: ',xA,', xB: ',xB
+		#print 'yA: ',yA,', yB: ',yB
+		row0	= np.matrix([xA,yA,1,0,0,0,-xA*xB,-yA*xB])
+		row1	= np.matrix([0,0,0,xA,yA,1,-xA*yB,-yA*yB])
+		M1 = np.vstack((M1,row0))
+		M1 = np.vstack((M1,row1))
+		M2[i*2,0]	= xB
+		M2[i*2+1,0]	= yB
+	#print M1
+	#print M2
+	H = np.linalg.inv(np.transpose(M1)*M1)*(np.transpose(M1)*M2)
+	#print H
+	H = np.vstack((H,[1]))
+	H = np.reshape(H,(3,3))
+	Hi = np.linalg.inv(H)
+	return (H,Hi,height_out,width_out,xmin_out,xmax_out,ymin_out,ymax_out)
+
+def hom0(image):
+## CV2 proprietary method # 0.003 seconds
+	(H,Hi,height_out,width_out,xmin_out,xmax_out, ymin_out,ymax_out) = calchomography(image)
+	t = time.time()
+	(height, width, channels) = image.shape
+	image_out	= cv2.warpPerspective(image,H,(width,height))
+	print 'Hom0: Elapsed time',time.time()-t
+	return image_out
+
+def hom1(image):
+# Own Loop backward homography # 2.06 seconds
+	t = time.time()
+	(H,Hi,height_out,width_out,xmin_out,xmax_out, ymin_out,ymax_out) = calchomography(image)
+	(height, width, channels) = image.shape
+	image_out = np.zeros((height_out,width_out,channels),dtype=np.uint8)
+	for h in range(ymin_out,ymax_out):
+		for w in range(xmin_out,xmax_out):
+			tmp = np.matrix([[w],[h],[1]])
+			res = Hi*tmp
+			scale	= res[2]
+			xtmp	= int(res[0]/scale)
+			ytmp	= int(res[1]/scale)
+			if xtmp<0 or xtmp>=width or ytmp<0 or ytmp>=height:
+				continue
 			else:
-				bx = fx
-				by = fy
-				#print 'bx0: ',bx
-				#print 'bx1: ',-f/d[2,0]*d[0,0]-0
-			# convert meters to pixels
-			bxp=bx/pitch+width_out/2
-			byp=by/pitch+height_out/2
-			#print 'bx: ',bx
-			#print 'by: ',by
-			#print 'bxp: ',bxp
-			#print 'byp: ',byp
-			X[yp,xp] = bxp # pixel coordinate on display
-			Y[yp,xp] = byp # pixel coordinate on display
+				#print 'y: %+5d -> %+5d'%(ytmp,h)
+				#print 'x: %+5d -> %+5d'%(xtmp,w)
+				#print image[ytmp,xtmp,:]
+				image_out[h-ymin_out,w-xmin_out,:] = image[ytmp,xtmp,:]
+				#print image_out[h,w,:]
+				#print image[ytmp,xtmp,:]
+	print 'Hom1: Elapsed time',time.time()-t
+	return image_out
 
-	height_out	= int(np.max(np.max(abs(Y))))
-	width_out	= int(np.max(np.max(abs(X))))
-	print height_out
-	print width_out
-	image_new	= np.zeros((height_out+1,width_out+1,channels),dtype=np.uint8)
-	print image_new.shape
-	# perform forward mapping for testing
-	# TODO: this must be a backward mapping if forward gives correct results
-	for x in range(width):
-		for y in range(height):
-			# TODO: perform interpolation instead of rounding
-			x_tmp = int(X[y,x])
-			y_tmp = int(Y[y,x])
-			#print 'x: ',x,'\t-> ',x_tmp
-			#print 'y: ',y,'\t-> ',y_tmp
-			#if x_tmp >= width or y_tmp >= height or x_tmp<0 or y_tmp < 0:
-			#	image_new[y,x,:] = 0
-			#else:
-				#print 'Before'
-				#print image[y,x,:]
-				#print image_new[y,x,:]
-			#x_tmp = abs(x_tmp)
-			#y_tmp = abs(y_tmp)
-			x_tmp = x_tmp
-			y_tmp = y_tmp
-			image_new[y_tmp,x_tmp,:] = copy.deepcopy(image[y,x,:])
-				#print 'After'
-				#print image[y,x,:]
-				#print image_new[y,x,:]
+def hom2(image):
+## Faster backward homography # 0.106 seconds
+	t = time.time()
+	(H,Hi,height_out,width_out,xmin_out,xmax_out, ymin_out,ymax_out) = calchomography(image)
+	(height, width, channels) = image.shape
+	# calc mapping
+	x = range(xmin_out,xmax_out)
+	y = range(ymin_out,ymax_out)
+	X, Y = np.meshgrid(x,y)
+	W = np.ones((height_out,width_out))
+	O = np.stack((X,Y,W),2)
+	map_out	= np.einsum('kp,ijp->ijk',Hi,O)
+	# perform mapping
+	image_out = np.zeros((height_out,width_out,channels),dtype=np.uint8)
+	for h in range(height_out):
+		for w in range(width_out):
+				scale = map_out[h,w,2]
+				x = int(round(map_out[h,w,0]/scale))
+				y = int(round(map_out[h,w,1]/scale))
+				if x<0 or x>=width or y<0 or y>=height:
+					continue
+				image_out[h,w,:] = image[y,x,:]
+	print 'Hom2: Elapsed time',time.time()-t
+	return image_out
 
-				#image_new = image
-	cv2.imshow('test',image_new)
-	cv2.waitKey(2000)
-	return 0
+def hom3(image):
+## Faster backward homography, mapping by masking and matrix indices method # 0.007 seconds
+	t = time.time()
+	(H,Hi,height_out,width_out,xmin_out,xmax_out, ymin_out,ymax_out) = calchomography(image)
+	(height, width, channels) = image.shape
+	# calc mapping
+	x = range(xmin_out,xmax_out)
+	y = range(ymin_out,ymax_out)
+	X, Y = np.meshgrid(x,y)
+	W = np.ones((height_out,width_out))
+	O = np.stack((X,Y,W),2)
+	map_out	= np.einsum('kp,ijp->ijk',Hi,O)
+	
+	map_out[:,:,1]	= map_out[:,:,1]/map_out[:,:,2]
+	map_out[:,:,0]	= map_out[:,:,0]/map_out[:,:,2]
+	map_out			= map_out.astype(int) # mapping must be of integer type because it is used directly for indexing
+	# construct empty image
+	image_out = np.zeros((height_out,width_out,channels),dtype=np.uint8)
+	# make conditional mask for the width (not larger than max image width)
+	mask_width = np.logical_and(map_out[:,:,0] < width,map_out[:,:,0]>=0)
+	# make conditional mask for the width (not larger than max image height)
+	mask_height = np.logical_and(map_out[:,:,1]>=0,map_out[:,:,1]<height)
+	# combine masks
+	mask_total = np.logical_and(mask_width==1,mask_height==1)
+	# Use mask to copy values from original image
+	image_out[mask_total,:] = image[map_out[mask_total,1],map_out[mask_total,0],:]
+	print 'Hom3: Elapsed time',time.time()-t
+	return image_out
 
 def main():
 	image		= cv2.imread(default_image)
-	windowname	= "image"
+	windowname	= "Original image"
 	cv2.namedWindow(windowname)
 	cv2.imshow(windowname,image)
-	cv2.waitKey(10)
-	windowname = 'test'
-	cv2.namedWindow(windowname)
+	cv2.waitKey(100)
 	
-	performhomography(windowname, image)
+	im0 = hom0(image)
+	im1 = hom1(image)
+	im2 = hom2(image)
+	im3 = hom3(image)
 
+	# Show results
+	cv2.imshow('Hom0',im0)
+	cv2.imshow('Hom1',im1)
+	cv2.imshow('Hom2',im2)
+	cv2.imshow('Hom3',im3)
+	cv2.waitKey(0)
+
+#	Hom0: Elapsed time 0.00331997871399
+#	Hom1: Elapsed time 2.2098929882
+#	Hom2: Elapsed time 0.0985150337219
+#	Hom3: Elapsed time 0.00763010978699
 	return 0
 
 if __name__ == '__main__':
