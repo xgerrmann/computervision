ad794df5e5717cb2995de4b290422e10f0d7df07
xgerrmann <xander@gerrmann.nl>
2016-08-09 22:08:03 +0200

    Plotting the facial landmakrs now goes through opencv instead through dlib.

--- main.py
+++ main.py
@@ -13,15 +13,36 @@ import copy
 
 ## Sources
 # Example:		http://dlib.net/face_landmark_detection.py.html
-# Speeding up:	http://www.learnopencv.com/speeding-up-dlib-facial-landmark-detector/
+# Speeding up:	http://www.learnopencv.com/speeding-up-dlib-facial-landmark-detector/
+def draw_polyline(img, shape, start, stop, isClosed=False):
+# directly from: http://www.learnopencv.com/speeding-up-dlib-facial-landmark-detector/
+	points = np.zeros((stop-start,2),dtype=np.int32)
+	for i in range(start,stop):
+		points[i-start,0] = shape.part(i).x
+		points[i-start,1] = shape.part(i).y
+	cv2.polylines(img,np.int32([points]),isClosed,[255,0,0])
 
-def detect(win,predictor,detector,frame):
+def showshape(window_face,frame,shape):
+# directly from: http://www.learnopencv.com/speeding-up-dlib-facial-landmark-detector/
+	draw_polyline(frame, shape, 0, 16);           # Jaw line
+	draw_polyline(frame, shape, 17, 21);          # Left eyebrow
+	draw_polyline(frame, shape, 22, 26);          # Right eyebrow
+	draw_polyline(frame, shape, 27, 31);          # Nose bridge
+	draw_polyline(frame, shape, 30, 35, True);    # Lower nose
+	draw_polyline(frame, shape, 36, 41, True);    # Left eye
+	draw_polyline(frame, shape, 42, 47, True);    # Right Eye
+	draw_polyline(frame, shape, 48, 59, True);    # Outer lip
+	draw_polyline(frame, shape, 60, 67, True);    # Inner lipt
+	cv2.imshow(window_face,frame)
+	cv2.waitKey(1)
+	return 0
+
+def detect_face(window_face,window_image,predictor,detector,frame):
 	# Ask the detector to find the bounding boxes of each face. The 1 in the
 	# second argument indicates that we should upsample the image 1 time. This
 	# will make everything bigger and allow us to detect more faces.
 	# Draw new frame
 	temp = time()
-	win.set_image(frame)
 	print 'Plot frame: ',time() - temp, 's'
 	# Detect face
 	temp = time()
@@ -47,18 +68,13 @@ def detect(win,predictor,detector,frame):
 	temp = time()
 	shape = predictor(frame, d)
 	print 'Predict: ',time() - temp, 's'
-	# Clear overlay
-	temp = time()
-	win.clear_overlay()
-	print 'Clear overlay: ',time() - temp, 's'
 	# Draw the face landmarks on the screen.
 	temp = time()
-	win.add_overlay(shape)
+	showshape(window_face,frame,shape)
 	print 'Add overlay: ',time() - temp, 's'
 
 	return shape
 
-# TODO: draw in openCV and not wit dlib (shape.parts())
 def shape2pose(shape_calibrated, shape_current):
 # This function determines the pose of the head by using the foudn markers on the face.
 # The current markers are compared to the calibrated markers and from this the head pose is determined.
@@ -88,240 +104,53 @@ def shape2pose(shape_calibrated, shape_current):
 	nb_length_cur = ymax - ymin
 
 	headpose.rx = math.cos(float(nb_length_cur)/float(nb_length_cal))
-	print nb_length_cur
-	print nb_length_cal
-	print	math.acos(float(nb_length_cur)/float(nb_length_cal))
+	#print nb_length_cur
+	#print nb_length_cal
+	#print	math.acos(float(nb_length_cur)/float(nb_length_cal))
 
 	return headpose
 
-def calcrotationmatrix(rx, ry, rz):
-	# source: http://nghiaho.com/?page_id=846
-	# source: https://en.wikipedia.org/wiki/3D_projection (uses negative angles?)
-	#rx = -rx
-	ry = -ry
-	rz = -rz
-	X	= np.matrix([[1, 0, 0],
-					[0,	math.cos(rx), -math.sin(rx)],
-					[0, math.sin(rx), math.cos(rx)]])
-	Y	= np.matrix([[math.cos(ry), 0, math.sin(ry)],
-					[0, 1, 0],
-					[-math.sin(ry), 0, math.cos(ry)]])
-	Z	= np.matrix([[math.cos(rz), -math.sin(rz), 0],
-					[math.sin(rz), math.cos(rz), 0],
-					[0, 0, 1]])
-#	# source (1): http://web.cs.iastate.edu/~cs577/handouts/homogeneous-transform.pdf
-#	# source (2) http://www.cs.virginia.edu/~gfx/Courses/2012/IntroGraphics/lectures/10-Transformations.pdf
-#	X	= np.matrix([[1, 0,				0,				0],
-#					[0,	 math.cos(rx),	-math.sin(rx),	0],
-#					[0,  math.sin(rx),	math.cos(rx),	0],
-#					[0,  0,				0,				1]])
-#	Y	= np.matrix([[math.cos(ry),	0,	math.sin(ry),	0],
-#					[0,				1,	0,				0],
-#					[-math.sin(ry),	0,	math.cos(ry),	0],
-#					[0,				0,	0,				1]])
-#	Z	= np.matrix([[math.cos(rz),	-math.sin(rz),	0,	0],
-#					[math.sin(rz),	math.cos(rz),	0,	0],
-#					[0,				0,				1,	0],
-#					[0,				0,				0,	1]])
-	R	= Z*Y*X
-	# rotation matrix applies only on X and Y, there is no Z. Therefore
-	# remove last row and column
-	#R	= R[0:2,0:2]
-	return R
-
-def getoutputimagesize(Tpers,homography,height,width):
-	# Corner points : [x,y,w]
-#	p0 = np.matrix([[0],[0],[1]])
-#	p1 = np.matrix([[width],[0],[1]])
-#	p2 = np.matrix([[0],[height],[1]])
-#	p3 = np.matrix([[width],[height],[1]])
-	# with z coordinate = 0
-	# w = 1
-	p0 = np.matrix([[0],[0],[0],[1]])
-	p1 = np.matrix([[width],[0],[0],[1]])
-	p2 = np.matrix([[0],[height],[0],[1]])
-	p3 = np.matrix([[width],[height],[0],[1]])
-	cornerpoints = [p0,p1,p2,p3]
-	xmin = np.inf
-	xmax = -np.inf
-	ymin = np.inf
-	ymax = -np.inf
-	for p in cornerpoints:
-		res = Tpers*homography*p
-		print p
-		print res
-		x_tmp = res[0]
-		y_tmp = res[1]
-		if x_tmp<xmin:
-			xmin=x_tmp
-		if y_tmp<ymin:
-			ymin=y_tmp
-		if x_tmp>xmax:
-			xmax=x_tmp
-		if y_tmp>ymax:
-			ymax=y_tmp
-	height_out	= int(np.ceil(ymax-ymin))
-	width_out	= int(np.ceil(xmax-xmin))
-	print 'Size image_out: ',height_out,', ',width_out
-	return (height_out, width_out)
-
 def adjustwindow(windowname,image,headpose):
-	rx	= 0.0*math.pi
-	ry	= 0.0*math.pi
-	rz	= 0.0*math.pi
-	R	= calcrotationmatrix(rx, ry, rz)
 	
-#	tx	= 0 # translation x
-#	ty	= 0 # translation y
-#	tz	= 0 # translation z
-
-	f = 0.4 # TODO: must be detected accurately for correct results
-
-	# e: viewers position relative to the camera
-	ex = 0
-	ey = 0
-	ez = -f
-	e = np.matrix([[-f],[0],[0]]) # viewer is direcly behind projection plane
-	# T: transformation matrix for projection of 3D -> 2D
-	T = np.zeros((4,4))
-	T[0,0] = 1
-	T[1,1] = 1
-	T[2,2] = 1
-	T[0,2] = -ex/ez
-	T[1,2] = -ey/ez
-	T[3,2] = 1/ez
-
-	print 'Rotation matrix: \n',R
-	print 'Transformation matrix: \n',T
-	
-#	hom_inv		= np.linalg.inv(homography)
-#	print 'Inverse homography:\n',hom_inv
-#	print image.shape
-	(height, width, channels) = image.shape
-
-	# determine size of resulting image:
-	#(height_out, width_out) = getoutputimagesize(T,R,height,width)
-	height_out	= height
-	width_out	= width
-	pitch = 0.2625*(10**-3) # [m] pixel pitch (pixel size) assume square pixels, which is generally true
-	print 'Pitch: %.6f'%pitch
-	w = 1
-	X = np.zeros((height_out,width_out))
-	Y = np.zeros((height_out,width_out))
-	Y = np.zeros((height_out,width_out))
-	W = np.zeros((height_out,width_out))
-	for xp in range(width_out):
-		#print x
-		for yp in range(height_out):
-			# a is the position of the point to be projected
-			# x and y depend on pixel size (screen dependent)
-			x = xp*pitch
-			y = yp*pitch
-			a = np.matrix([[x],[y],[0]]) # virtual image is always at z=0
-			print 'a: ',a
-			# c is the 3D position of the camera
-			c = np.matrix([[0],[0],[0]])
-			print 'c: ',c
-			# d is the position of point A with respect to a coordinate system
-			dtmp = R*(a-c)
-			# convert d to homogeneous coordinates
-			print dtmp
-			d = np.matrix(np.zeros((4,1)))
-			d[0:3,0]= dtmp[0:3,0]
-			d[3,0]	= 1
-			print d
-			# 3D -> 2D
-			f = T*d
-			print 'f: ',f
-			fx = f[0]
-			fy = f[1]
-			fz = f[2]
-			fw = f[3]
-			print 'fx: ',float(fx)
-			print 'fy: ',float(fy)
-			print 'fw: ',float(fw)
-			if fw<>0.0: # TODO: why is fw always zeros? only in euqal plane scenario?
-				print 'True'
-				bx = fx/fw
-				by = fy/fw
-			else:
-				bx = fx
-				by = fy
-			# convert meters to pixels
-			bxp=bx/pitch
-			byp=by/pitch
-			print 'bx: ',bx
-			print 'by: ',by
-			print 'bxp: ',bxp
-			print 'byp: ',byp
-			X[yp,xp] = bxp # pixel coordinate on display
-			Y[yp,xp] = byp # pixel coordinate on display
-
-	height_out	= np.max(np.max(Y))
-	width_out	= np.max(np.max(X))
-	image_new	= np.zeros((height_out,width_out,channels),dtype=np.uint8)
-	# perform forward mapping for testing
-	# TODO: this must be a backward mapping if forward gives correct results
-	for x in range(width):
-		for y in range(height):
-			# TODO: perform interpolation instead of rounding
-			x_tmp = int(X[y,x])
-			y_tmp = int(Y[y,x])
-			#print 'x: ',x,'-> ',x_tmp
-			#print 'y: ',y,'-> ',y_tmp
-			#if x_tmp >= width or y_tmp >= height or x_tmp<0 or y_tmp < 0:
-			#	image_new[y,x,:] = 0
-			#else:
-				#print 'Before'
-				#print image[y,x,:]
-				#print image_new[y,x,:]
-			image_new[x_tmp,y_tmp,:] = copy.deepcopy(image[y,x,:])
-				#print 'After'
-				#print image[y,x,:]
-				#print image_new[y,x,:]
-
-				#image_new = image
-	cv2.imshow('test',image_new)
-	cv2.waitKey(1)
+	#cv2.imshow('Image transformed',image_new)
+	#cv2.waitKey(1)
 	return 0
 
 def main():
 	detector	= dlib.get_frontal_face_detector()
-	print trained_model
 	predictor	= dlib.shape_predictor(trained_model)
 	image		= cv2.imread(default_image)
-	windowname = "image"
-	cv2.namedWindow(windowname)
-	cv2.imshow(windowname,image)
+	window_image = "Image"
+	cv2.imshow(window_image,image)
 	cv2.waitKey(1)
-	windowname = 'test'
-	cv2.namedWindow(windowname)
+	window_face = 'Face'
+	cv2.namedWindow(window_face)
 	vc = cv2.VideoCapture(0)
-	win_dlib = dlib.image_window()
+	#win_dlib = dlib.image_window()
 	if vc.isOpened(): # try to get the first frame
 		rval, frame = vc.read()
 	else:
-		rval = False
+		sys.exit('No frame captured')
 
-	while rval:
+	while True:
 		#cv2.imshow("preview", frame)
 		rval, frame = vc.read()
 		key = cv2.waitKey(1)
 		if key == 27: # exit on ESC
 			break
-		shape = detect(win_dlib,predictor,detector,frame)
-		if shape == 0:
+		shape = detect_face(window_face,window_image,predictor,detector,frame)
+		if shape == 0: # In the case of no detection, continue to the next frame
+			cv2.imshow(window_face,frame)
+			cv2.waitKey(1)
 			continue
 		else:
 			# TODO: calibrated shape is now same as current, calibrated shape must be indicated by used.
 			shape_calibrated= shape
 			shape_current	= shape
 			headpose		= shape2pose(shape_calibrated, shape_current)
-			print headpose
-			adjustwindow(windowname, image, headpose)
+			adjustwindow(window_image, image, headpose)
 
 	vc.release()
-	#cv2.destroyWindow("preview")
 
 	return 0
 
