9f2ba51b277b88b25c01faad88f3f5312aeb350c
xgerrmann <xander@gerrmann.nl>
2016-09-10 10:58:03 +0200

    Finishscript for plotting time vs size.

--- analyses/speedcurve.py
+++ analyses/speedcurve.py
@@ -2,7 +2,8 @@
 
 import os
 import numpy as np
-import cv
+import cv2
+import matplotlib.pyplot as plt
 
 def main():
 	# define paths
@@ -14,10 +15,28 @@ def main():
 	sizedata = np.loadtxt(open(sizefile,"rb"),delimiter=",")
 	timedata = np.loadtxt(open(timefile,"rb"),delimiter=",")
 
-	print sizedata
-	print timedata
+	timedata = 1000*timedata
+	
+	# calculate mean and standard deviation
+	mean_time = timedata.mean(axis=0);
+	stdev_time = timedata.std(axis=0);
+	
+	size	= [elem[0]*elem[1] for elem in sizedata]
+	
+	t_max	= mean_time+stdev_time
+	t_min	= mean_time-stdev_time
+	# show data
+	plt.figure(facecolor='white')
+	plt.fill_between(size, t_min, t_max, facecolor='blue', interpolate=True,alpha = 0.5)
+	plt.semilogx(size,mean_time, color='blue',linewidth=1)
+	plt.title('Computational time vs number of pixels')
+	plt.ylim([0,1])
+	plt.xlim([min(size),max(size)])
+	plt.xlabel("Size [pixels]")
+	plt.ylabel("Time [ms]")
+	#plt.show()
 
-
-
+	# save plot
+	plt.savefig('media/plots/size-time.png', facecolor="white")
 if __name__ == "__main__":
 	main()
